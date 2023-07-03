7502b719c7277a74865f73e85a718070a380a9b1
thilaks-hash <thilagavathyr025@gmail.com>
2023-07-04 00:31:34 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,39 @@
-import React from 'react'
-import '../styles/App.css';
-import { Loader } from './Loader';
-import { PhotoFrame } from './PhotoFrame';
+import React, { useEffect, useState } from "react";
+import "../styles/App.css";
+import { Loader } from "./Loader";
+import { PhotoFrame } from "./PhotoFrame";
 const App = () => {
-  
-}
+  const [number, setNumber] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [photo, setPhoto] = useState(null);
+  const getPhoto = async () => {
+    if (!number) return;
+    setIsLoading(true);
+    const respone = await fetch(
+      ` https://jsonplaceholder.typicode.com/photos/${number}`
+    );
+    const data = await respone.json();
+    console.log(data);
+    setPhoto(data);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getPhoto();
+  }, [number]);
+  const handleInput = (e) => {
+    setNumber(e.target.value);
+  };
 
+  return (
+    <div>
+      <input type="number" value={number} onChange={handleInput}></input>
+      {isLoading ? (
+        <Loader />
+      ) : photo ? (
+        <PhotoFrame url={photo.url} title={photo.title} />
+      ) : null}
+    </div>
+  );
+};
 
 export default App;
--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React from "react";
 
-export const PhotoFrame = () => {
-   
-}
+export const PhotoFrame = ({ url, title }) => {
+  return (
+    <div className="photoframe">
+      <img src={url} alt="Photo" />
+      <div className="caption">{title}</div>
+    </div>
+  );
+};
